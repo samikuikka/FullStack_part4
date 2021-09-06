b52b114d50ac08bef10936e9d168498debd9a8a1
Sami Kuikka <sami.kuikka@aalto.fi>
2021-09-06 09:22:00 +0300

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? []
+    : blogs.reduce((best, current) => {
+      return current.likes > best.likes ? current : best
+    }, {likes: -1})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
