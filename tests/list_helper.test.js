ff86f59276fb98b52939a7cf83ed7d2ab28f91fb
Sami Kuikka <sami.kuikka@aalto.fi>
2021-09-06 08:52:32 +0300

    4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,50 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('totaLikes', () => {
+
+  test('of no blogs is zero', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+  test('when only one blog, the likes of amount of that', () => {
+    let blogs = [
+      {
+        _id: '61310ffaa56d4646fc9fcfab',
+        title: 'My blog',
+        author: 'Loon',
+        url: 'http://loon-blog.com',
+        likes: 1572,
+        __v: 0 
+      }
+    ]
+
+    expect(listHelper.totalLikes(blogs)).toBe(1572)  
+  })
+
+  test('in bigger blog list result is sum of all blogs', () => {
+    let blogs = [
+      {
+        title: 'My blog',
+        author: 'Loon',
+        url: 'http://loon-blog.com',
+        likes: 1572,
+      },
+      {
+        title: 'My second blog',
+        author: 'Loon',
+        url: 'http://loon-blog2.com',
+        likes: 146,
+      },
+      {
+        title: 'Awesome blog',
+        author: 'Mr. Smith',
+        url: 'http://awesomeblog.com',
+        likes: 1,
+      }
+    ]
+
+    expect(listHelper.totalLikes(blogs)).toBe(1719)
+  })
+})
