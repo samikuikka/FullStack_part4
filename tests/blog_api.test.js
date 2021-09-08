a302575063b1aa4f5a28a8823a9b319b56797ad7
Sami Kuikka <sami.kuikka@aalto.fi>
2021-09-08 13:35:15 +0300

    4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -30,13 +30,35 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
-test.only('all blogs have an property id', async () => {
+test('all blogs have an property id', async () => {
   const response = await api.get('/api/blogs')
   let flag = true
   response.body.forEach(blog => {
     if(!Object.prototype.hasOwnProperty.call(blog, '_id')) flag = false
   })
   expect(flag).toBe(true)
+})
+
+test.only('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'How to add blogs',
+    author: 'Paul',
+    url: 'http://paulblogging.com',
+    likes: 100
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = await helper.blogsInDb()
+  console.log(blogs)
+  expect(blogs).toHaveLength(helper.initialBlogs.length +1)
+
+  const content = blogs.map(x => x.title)
+  expect(content).toContain('How to add blogs')
 })
 
 afterAll(() => {
